91a09f21388749c435fe091851c646fa11076ca4
Jason <jasonalvarezcoraiza@gmail.com>
2020-01-22 18:31:18 -0500

    added a ship component with two props

---
+++ src/ship.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Ship extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            health: props.length,
+            cord: []
+        }
+    }
+
+    setCords(cordinates) {
+        this.setState({
+            health: this.props.length,
+            cord: cordinates,
+        });
+    }
+    render() {
+        return (
+            <div onClick>
+                Ship:
+        length: {this.props.length}
+                health: {this.state.health}
+                orientation: {this.props.orientation}
+            </div>
+        );
+    }
+}
+
+export default Ship;
--- src/shipContainer.jsx
+++ src/shipContainer.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Ship from "./ship.jsx";
 
 class ShipContainer extends Component {
   render() {
     return (
-      <h1>Ship components</h1>
+      <div>
+        <h1>Ship components</h1>
+        <Ship length={5} orientation={"horizontal"}/>
+        <Ship length={5}/>
+        <Ship length={3}/>
+        <Ship length={3}/>
+        <Ship length={2}/>
+      </div>
+
     );
   }
 }
