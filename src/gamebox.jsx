caa0f938df9b9c39e2b8cb35ea6f2248ff1831fd
Jason <jasonalvarezcoraiza@gmail.com>
2020-02-05 17:38:32 -0500

    added state to game box

--- src/gamebox.jsx
+++ src/gamebox.jsx
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
 
 class GameBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      active: "false"
+    }
+  }
+
+  shoot(){
+    console.log("hi");
+    this.setState({
+      active: "true"
+    });
+  }
+
   render() {
+    
     let styles= {
       width: "30px",
       height: "30px",
       border: "1px solid",
       float: 'left'
     };
+    
+    if(this.state.active) {
+      let styles= {
+        width: "30px",
+        height: "30px",
+        backgroundColor: "red",
+        border: "1px solid",
+        float: "left"
+      }
+    };
+
     return (
-      <div style={styles}>
+      <div style={styles} onClick={()=>this.shoot}>
       </div>
 
     );
