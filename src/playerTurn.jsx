49c8d258e5c08def2bdb5bd17315914f45715ee2
cgupt <christopher.gupta@aexp.com>
2020-02-05 17:47:38 -0500

    player turn

--- src/playerTurn.jsx
+++ src/playerTurn.jsx
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class PlayerTurn extends Component {
-  render() {
-    return (
-      <h1>Player Turn</h1>
-    );
-  }
+function PlayerTurn (props){
+
+    
+      const [ player,setPlayer
+      ] =
+      useState('player1');
+      return (<>
+      <h1>{player} Turn</h1>
+      <button onClick= {()=>{
+        if (player === 'player1'){
+        setPlayer ('player2')};
+        if (player === 'player2'){
+          setPlayer ('player1')
+        };
+      }}>Click me!</button>
+      </>)
 }
 
 export default PlayerTurn;
